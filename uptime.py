b883940d218a340a3a3c4ca85bad48b36c578786
Brenden Sweetman <brenden.sweetman@wustl.edu>
2018-04-10 13:43:39 -0500

    first working (hopefully) backend

--- uptime.py
+++ uptime.py
@@ -12,13 +12,13 @@ import datetime
 import pandas as pd
 import json
 def main():
-    requests()
+    httpCheck()
     parseShort()
     parseLong()
     deleteOld()
 
 
-def requests():
+def httpCheck():
     domains=[]
     dbCon = connector.connect(user="uptime", password="vNg&.+4]6h2nuR00", host='localhost', database="uptime")
     #Select all domains from database
@@ -26,7 +26,6 @@ def requests():
     cursor.execute("SELECT id, name, url, ip FROM domains")
     for (id, name, url, ip) in cursor:
         domains.append((id,name,url,ip))
-    cursor.close()
     for domain in domains:
         requestData = ()
         errorMessage = ""
@@ -55,52 +54,53 @@ def requests():
             pingTime= re.findall(r"time=([\d\.]+)",pingResults)[0]
 
         requestData=(domain[0],statusCode,errorMessage,pingTime,httpTime)
-        cursor = dbCon.cursor()
         cursor.execute("INSERT INTO requests (domain_id,status_code,error_message,ping_time,http_time) VALUES (%s,%s,%s,%s,%s)",requestData)
-        dbCon.commit()
-        cursor.close()
-        dbCon.close()
-    threading.Timer(60,requests).start()
+    dbCon.commit()
+    cursor.close()
+    dbCon.close()
+    threading.Timer(60,httpCheck).start()
 def parseShort():
+    parse(1200,"shortData.json")
+    threading.Timer(60,parseShort).start()
+def parseLong():
+    parse(604800,"longData.json")
+    threading.Timer(40000,parseLong).start()
+def parse(delta,fileName):
     dbCon = connector.connect(user="uptime", password="vNg&.+4]6h2nuR00", host='localhost', database="uptime")
-    timeDelta = datetime.timedelta(min=20)
+    timeDelta = datetime.timedelta(seconds=delta)
     nowDateString = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     thenDateString = (datetime.datetime.today()-timeDelta).strftime('%Y-%m-%d %H:%M:%S')
     queryString = "SELECT domains.name as name,access_time,status_code,error_message,ping_time,http_time FROM requests JOIN domains ON requests.domain_id=domains.id WHERE access_time BETWEEN '{:s}' AND '{:s}' ORDER BY requests.access_time DESC".format(thenDateString,nowDateString)
     data = pd.read_sql(queryString, con=dbCon)
     dbCon.close()
     jsonData={}
-    print( "Start of short parse: ")
     names = data.name.unique()
-    print("Names:")
-    print(names)
     for name in names:
-        print("Starting :" + name)
         nameData=data.loc[data['name']==name]
         nameJsonData={}
         # Is site up?
         statusCounts = pd.value_counts(nameData['status_code'].values)
-        print("Status code counts:")
-        print(statusCounts)
-        if statusCounts['200']!= None:
-            nameJsonData["status"] = statusCounts['200']/nameData.shape[0]
-        else: nameJsonData["status"] = 0
+        try:
+            nameJsonData["status"] = statusCounts[200]/nameData.shape[0]
+        except: nameJsonData["status"] = 0
         # Whats the average ping timeDelta
-        print ("Average ping time")
-        print (nameData['ping_time'].mean())
         nameJsonData["meanPingTime"] = nameData['ping_time'].mean()
         # Whats the average http timeDelta
-        print ("Average http time")
-        print (nameData['http_time'].mean())
         nameJsonData["meanHttpTime"] = nameData['http_time'].mean()
         # add to main json
         jsonData[name] = nameJsonData
-    print ("Final json:")
-    print (jsonData)
-    threading.Timer(60,parseShort).start()
-def parseLong():
-    threading.Time(40000,parseLong).start()
+    with open("fileName", 'w') as file:
+        json.dump(jsonData,file)
 def deleteOld():
+    dbCon = connector.connect(user="uptime", password="vNg&.+4]6h2nuR00", host='localhost', database="uptime")
+    cursor = dbCon.cursor()
+    timeDelta = datetime.timedelta(days=30)
+    dateString = (datetime.datetime.today()-timeDelta).strftime('%Y-%m-%d %H:%M:%S')
+    queryString = "DELETE FROM requests WHERE access_time < '{:s}'".format(dateString)
+    cursor.execute(queryString)
+    dbCon.commit()
+    cursor.close()
+    dbCon.close()
     threading.Timer(86400,deleteOld).start()
 
 if __name__=="__main__":
