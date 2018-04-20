f0829e1b4e1fa2991660db33e46053bb5eabd70a
baldpixels <baldpixels@gmail.com>
2018-04-20 13:03:39 -0500

    JSON data-loading works; chart loads; layout cleaned up

--- js/overview.js
+++ js/overview.js
@@ -1,14 +1,10 @@
 /***** VARIABLES *****/
 moment().format();
 
-var services = [];
-
 var today = moment();
 
 /***** EVENT LISTENERS *****/
-$('.service_name a').click( function(e) {
 
-});
 
 /***** FUNCTIONS ******/
 function updateServiceData() {
@@ -18,40 +14,37 @@ function updateServiceData() {
   dataRequest.onload = function() {
     var serviceData = JSON.parse(dataRequest.responseText);
 
-    var i = 0;
-    while(serviceData[i] != null) {
-      services.push = serviceData[i];
-      i++;
-    }
-
-    displayServices();
+    displayServices(serviceData);
   };
 
   dataRequest.send(null);
 }
 
-function displayServices() {
-  services.forEach( function (service) {
+function displayServices(serviceData) {
+  var i = 0;
+  while(serviceData[i] != null) {
+    var service = serviceData[i];
+    var currRating = 1;
     $('#services_wrapper').append('\
       <div class="service">\
-        <div class="col1"><h3 class="service_name"><a href="details.html/?id=' + service.id + '">' + service.name +'</a></h3></div>\
-        <div class="col2"><span class="current_status">Uptime in Last 20 Minutes... <span class="status" id="' + service.id + '_status">' + Math.round(service.status*100) + '%</span></span></div>\
-        <div class="col3"><span></span></div>\
-        <div class="col4"><span></span></div>\
-        <div class="col5"><span></span></div>\
+        <div class="col1"><h3 class="service_name"><a href="details.html/?id=' + i + '">' + service.name +'</a></h3></div>\
+        <div class="col2"><span class="status" id="' + i + '_status">' + Math.round(service.status*100) + '%</span></div>\
+        <div class="col3"><span class="mean_http" id="' + i + '_mean_http">' + Math.round(service.meanHttpTime) + 'ms</span></div>\
+        <div class="col4"><span class="mean_ping" id="' + i + '_mean_ping">' + Math.round(service.meanPingTime) + 'ms</span></div>\
       </div>\
     ');
 
     if(service.status === 1) {
-      $('#' + service.id + '_status').css('background-color', '#7FFF00');
+      $('#' + i + '_status').css('background-color', '#7FFF00');
     }
     else if(service.status === 0) {
-      $('#' + service.id + '_status').css('background-color', '#FFC125');
+      $('#' + i + '_status').css('background-color', '#FFC125');
     }
     else {
-      $('#' + service.id + '_status').css('background-color', '#f2ff42');
+      $('#' + i + '_status').css('background-color', '#f2ff42');
     }
-  });
+    i++;
+  }
 }
 
 /***** MAIN RUN *****/
--- js/specific.js
+++ js/specific.js
@@ -3,88 +3,16 @@ moment().format();
 
 var selectedService = {};
 
-var services = [
-  {
-    name: 'Ares',
-    id: 'ares',
-    status: 0,
-    data: [],
-    domain: 'ares.wustl.edu',
-    ip: '128.252.67.118'
-  }, {
-    name: 'Classic Catalog',
-    id: 'classic_catalog',
-    status: 0,
-    data: [],
-    domain: 'catalog.wustl.edu',
-    ip: '128.252.67.17'
-  },/* {
-    name: 'Digital Gateway',
-    id: 'digital_gateway',
-    status: 0,
-    data: [],
-    domain: 'digital.wustl.edu',
-    ip: '128.252.67.14'
-  }, {
-    name: 'Findit',
-    id: 'findit',
-    status: 0,
-    data: [],
-    domain: '',
-    ip: ''
-  },*/ {
-    name: 'Illiad',
-    id: 'illiad',
-    status: 0,
-    data: [],
-    domain: 'illiad.wustl.edu',
-    ip: '128.252.67.41'
-  }, /*{
-    name: 'Libguides',
-    id: 'libguides',
-    status: 0,
-    data: [],
-    domain: 'libguides.wustl.edu',
-    ip: '52.54.77.227'
-  }, {
-    name: 'OCLC WorldCat',
-    id: 'worldcat',
-    status: 0,
-    data: [],
-    domain: 'www.worldcat.org',
-    ip: '132.174.0.31'
-  }, {
-    name: 'Primo',
-    id: 'primo',
-    status: 0,
-    data: [],
-    domain: 'wash-primo.hosted.exlibrisgroup.com',
-    ip: '66.151.7.251'
-  },*/ {
-    name: 'Repository (Samvera)',
-    id: 'sam_repo',
-    status: 0,
-    data: [],
-    domain: 'repository.wustl.edu',
-    ip: '128.252.67.231'
-  }, {
-    name: 'Streaming Video (Samvera)',
-    id: 'sam_video',
-    status: 0,
-    data: [],
-    description: 'streamingvideo.wustl.edu',
-    ip: '128.252.67.8'
-  }
-];
-
 var today = moment();
 var prevDay;
 var lastWeek = [];
 
+/* FIX DATE FORMATTING FOR CHART AXIS-LABEL */
 for(var i=1; i<8; i++) {
   today = moment();
   prevDay = today.subtract(i, 'days');
-  lastWeek.push(prevDay.format('MMMM') + ' ' + prevDay.date());
+  lastWeek.push(prevDay.format('YYYY-MM-DD'));
+  //prevDay.format('MMMM') + ' ' +
 }
 
 /***** FUNCTIONS ******/
@@ -98,29 +26,32 @@ function urlData(param) {
     }
 }
 
-function updateServiceData(service) {
-  /*
+function updateServiceData(serviceId) {
   var dataRequest = new XMLHttpRequest();
-  dataRequest.open('GET', 'data/longData.json', true);
+  dataRequest.open('GET', '/sampleJsonFiles/longDataExample.json', true);
 
   dataRequest.onload = function() {
     var serviceData = JSON.parse(dataRequest.responseText);
 
-    selectedService.data = serviceData[selectedService.name].data;
-
-    displayDetails();
+    displayDetails(serviceData);
   };
 
   dataRequest.send(null);
-  */
 }
 
-function displayDetails() {
-  loadChart();
+function displayDetails(serviceData) {
+  loadChart(serviceData);
 }
 
-function loadChart() {
-  new Chart($('#' + selectedService.id +'_chart'), {
+function loadChart(serviceData) {
+  var selectedService = serviceData[serviceId];
+
+  var serviceDataByDay = [];
+  for(var i=0; i<7; i++) {
+    serviceDataByDay.push(selectedService[lastWeek[i]].status);
+  }
+
+  new Chart($('.service_chart'), {
     type: 'bar',
     data: {
       labels: [lastWeek[6], lastWeek[5], lastWeek[4], lastWeek[3], lastWeek[2], lastWeek[1], lastWeek[0]],
@@ -128,12 +59,12 @@ function loadChart() {
         {
           label: 'Uptime (%)',
           backgroundColor: '#7FFF00',
-          data: selectedService.data
+          data: [(100*serviceDataByDay[6]), (100*serviceDataByDay[5]), (100*serviceDataByDay[4]), (100*serviceDataByDay[3]), (100*serviceDataByDay[2]), (100*serviceDataByDay[1]), (100*serviceDataByDay[0])]
         },
         {
           label: 'Downtime (%)',
           backgroundColor: '#FFC125',
-          data: [(100 - selectedService.data[0]), (100 - selectedService.data[1]), (100 - selectedService.data[2]), (100 - selectedService.data[3]), (100 - selectedService.data[4]), (100 - selectedService.data[5]), (100 - selectedService.data[6])]
+          data: [(100 - 100*serviceDataByDay[6]), (100 - 100*serviceDataByDay[5]), (100 - 100*serviceDataByDay[4]), (100 - 100*serviceDataByDay[3]), (100 - 100*serviceDataByDay[2]), (100 - 100*serviceDataByDay[1]), (100 - 100*serviceDataByDay[0])]
         }
       ]
     },
@@ -161,10 +92,6 @@ function loadChart() {
 /***** MAIN RUN *****/
 var serviceId = urlData('id');
 
-services.forEach( function (service) {
-  if(service.id === serviceId) {
-    selectedService = service;
-  }
-});
+updateServiceData(serviceId);
 
 updateServiceData();
